1a76cabadc013bd46960173e1042f7d45cfb4418
Christa Hegedus <christa.hegedus1@gmail.com>
2018-07-20 12:29:56 -0400

    Styled Components

--- src/Movie.js
+++ src/Movie.js
@@ -1,15 +1,14 @@
 import React from 'react';
+import styled from 'styled-components';
 import PropTypes from 'prop-types';
 import { Link } from 'react-router-dom';
 
 const POSTER_PATH = 'http://image.tmdb.org/t/p/w154';
 
 const Movie = ({ movie }) => (
-  <div>
     <Link to={`/${movie.id}`}>
-      <img src={`${POSTER_PATH}${movie.poster_path}`} alt={movie.title} />
+      <Poster src={`${POSTER_PATH}${movie.poster_path}`} alt={movie.title} />
     </Link>
-  </div>
 )
 
 export default Movie;
@@ -19,3 +18,7 @@ Movie.propTypes = {
     title: PropTypes.string.isRequired,
   }).isRequired,
 };
+
+export const Poster = styled.img `
+  box-shadow: 0 0 35px black;
+`;
